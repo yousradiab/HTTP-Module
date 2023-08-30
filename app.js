5b3841695ba8547036c52b2e78fec1bd1cb6b989
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-08-30 11:55:56 +0200

    POST til user

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-import { users } from "./users.js";
-import { posts } from "./posts.js";
-
 import http from "node:http";
 
-console.log(posts);
+import fs from "fs/promises";
 
-const app = http.createServer((request, response) => {
+const app = http.createServer(async (request, response) => {
   if (request.url === "/" && request.method === "GET") {
-
     response.statusCode = 200;
 
     response.setHeader("Content-Type", "text/plain");
     response.end("Working with HTTP Module and routing");
-    
   } else if (request.url === "/users" && request.method === "GET") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json");
 
-    response.end(JSON.stringify(users));
+    const json = await fs.readFile("users.json");
+    response.end(json);
+  } else if (request.url === "/users" && request.method === "POST") {
+    const user = {
+      id: new Date().getTime(),
+      image: "https://e1.pngegg.com/pngimages/727/616/png-clipart-hamtaro-brown-and-white-rodent-illustration.png",
+      mail: "test@test.dk",
+      name: "Hamtaro Test",
+      title: "Senior Hamster",
+    };
+    const json = await fs.readFile("./users.json");
+    console.log(json);
 
-  } else if (request.url ==="/posts" && request.method ==="GET") {
+    const users = JSON.parse(json);
+    console.log(users);
+    users.push(user);
+
+    const usersJSON = JSON.stringify(users);
+    await fs.writeFile("./users.json", usersJSON);
+
     response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json")
-    response.end(JSON.stringify(posts))
+    response.setHeader("Content-Type", "application/json");
+    response.end(usersJSON);
+  } else if (request.url === "/posts" && request.method === "GET") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "application/json");
+
+    const json = await fs.readFile("posts.json");
+    response.end(json);
   }
 });
 
